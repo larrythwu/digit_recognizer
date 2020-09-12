9137e3a448e18551afa1052ef2347aa456325ca1
larrythwu <larrythwu@gmail.com>
2020-09-11 21:53:04 -0400

    Updates

---
+++ precache-manifest.9c851896841b3b06d895b68ff5cf9997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9cc370e23331a3d0e4167d46fb7803",
+    "url": "/digit_recognizer/index.html"
+  },
+  {
+    "revision": "056af6af86ad3ba2148e",
+    "url": "/digit_recognizer/static/css/main.9fc82540.chunk.css"
+  },
+  {
+    "revision": "0852e1280c36935e0ff0",
+    "url": "/digit_recognizer/static/js/2.18b10121.chunk.js"
+  },
+  {
+    "revision": "fff58a980364af84ab3df241ff862c34",
+    "url": "/digit_recognizer/static/js/2.18b10121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "056af6af86ad3ba2148e",
+    "url": "/digit_recognizer/static/js/main.ab9c7ea8.chunk.js"
+  },
+  {
+    "revision": "4a1729db032cd601084f",
+    "url": "/digit_recognizer/static/js/runtime-main.9ea5f2a6.js"
+  }
+]);
